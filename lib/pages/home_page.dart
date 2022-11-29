df1bd1403e71e63794be2fad82fe4e1a7617115d
jazmin <correo@example>
2022-11-29 16:44:29 -0500

    commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatelessWidget {
+  //Uri
+  //Url
+  getDataPokemon() async {
+    Uri _uri = Uri.parse(
+        "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
+
+    http.Response response = await http.get(_uri);
+    print(response.statusCode);
+    print(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(14.0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              const SizedBox(
+                height: 12.0,
+              ),
+              const Text(
+                "Pokedex",
+                style: TextStyle(fontSize: 34.0, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              GridView.count(
+                shrinkWrap: true,
+                crossAxisCount: 2,
+                mainAxisSpacing: 12.0,
+                crossAxisSpacing: 12.0,
+                childAspectRatio: 1.35,
+                children: [
+                  Container(
+                    color: Colors.red,
+                  ),
+                ],
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
