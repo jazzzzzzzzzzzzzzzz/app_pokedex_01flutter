7a9c270cbb1bed92cda6b12350d0807ed67c50f1
jazmin <correo@example>
2022-11-29 19:20:27 -0500

    item

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:app_pokedex_01flutter/wit/widgets/item_pokemon_widget.dart';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
@@ -50,68 +51,10 @@ class HomePage extends StatelessWidget {
                 crossAxisSpacing: 12.0,
                 childAspectRatio: 1.35,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 207, 85, 76),
-                        borderRadius: BorderRadius.circular(18.0)),
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          bottom: -20,
-                          right: -20,
-                          child: Image.asset(
-                            'assets/images/pokebal.png',
-                            height: 120.0,
-                            color: Colors.white.withOpacity(0.27),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 20.0, horizontal: 12.0),
-                          child: Column(
-                            children: [
-                              Text(
-                                "Moltres",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Container(
-                                margin:
-                                    const EdgeInsets.symmetric(vertical: 6.0),
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 14.0, vertical: 4.0),
-                                decoration: BoxDecoration(
-                                  color: Colors.white.withOpacity(0.27),
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  boxShadow: [
-                                    BoxShadow(
-                                        color: Colors.white.withOpacity(0.04),
-                                        offset: const Offset(4, 4),
-                                        blurRadius: 12.0)
-                                  ],
-                                ),
-                                child: Text(
-                                  "Dark",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 0,
-                          right: 0,
-                          child: Image.network(
-                              "https://serebii.net/swordshield/pokemon/small/146-g.png"),
-                        ),
-                      ],
-                    ),
-                  ),
+                  ItemPokemonWidget(),
+                  ItemPokemonWidget(),
+                  ItemPokemonWidget(),
+                  ItemPokemonWidget(),
                 ],
               ),
             ]),
--- lib/wit/widgets/item_pokemon_widget.dart
+++ lib/wit/widgets/item_pokemon_widget.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
 
-class ItemPoke extends StatelessWidget {
-  const ItemPoke({Key? key}) : super(key: key);
+class ItemPokemonWidget extends StatelessWidget {
+  const ItemPokemonWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+          color: Color.fromARGB(255, 207, 85, 76),
+          borderRadius: BorderRadius.circular(18.0)),
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: -20,
+            right: -20,
+            child: Image.asset(
+              'assets/images/pokebal.png',
+              height: 120.0,
+              color: Colors.white.withOpacity(0.27),
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.0, horizontal: 12.0),
+            child: Column(
+              children: [
+                Text(
+                  "Moltres",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 6.0),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 14.0, vertical: 4.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.27),
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.white.withOpacity(0.04),
+                          offset: const Offset(4, 4),
+                          blurRadius: 12.0)
+                    ],
+                  ),
+                  child: Text(
+                    "Dark",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Image.network(
+                "https://serebii.net/swordshield/pokemon/small/146-g.png"),
+          ),
+        ],
+      ),
+    );
   }
 }
